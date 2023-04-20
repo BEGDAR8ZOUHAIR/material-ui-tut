5a2452d9da3991c19143af8cc5cf3a8dbbdcd939
BEGDAR8ZOUHAIR <begdar8zouhair@gmail.com>
2023-04-20 09:41:36 +0000

    setup

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { createMuiTheme, ThemeProvider } from '@material-ui/core'
+import { purple } from '@material-ui/core/colors'
+import Layout from './components/Layout'
+import CreateTable from './pages/CreateTable' 
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: '#fefefe'
+    },
+    secondary: purple
+  },
+  typography: {
+    fontFamily: 'Quicksand',
+    fontWeightLight: 400,
+    fontWeightRegular: 500,
+    fontWeightMedium: 600,
+    fontWeightBold: 700,
+  }
+})
+
+function App() {
+  return (
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route exact path="/">
+            </Route>
+            <Route path="/">
+              <CreateTable />
+            </Route>
+          </Switch>
+        </Layout>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
